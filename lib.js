1277d352e8173dff05342386f028643e4d7ae05f
Rashedul Hassan <rashedul@outlook.com>
2020-02-26 22:32:16 +0000

    Added comments

--- lib.js
+++ lib.js
@@ -1,26 +1,30 @@
 
-
 function hello()
 {
     console.log('hello world');
     return 42;
 }
 
+
 function requestMultipleUrls(urls)
 {
+    //  Check for null params
     if (!urls)
         throw new Error('A set of Url/s is required');
 
     const all_my_promises = [];
 
+    //  Loop through all the urls given to the function
     for (var i = 0; i < urls.length; i++)
     {
         const one_promise = new Promise(function(resolve, reject)
         {
+            //  Pick https if url consists of https, or http if url consist of http
             const library_to_use = urls[i].startsWith('https') ? require('https') : require('http');
 
             const request = library_to_use.get(urls[i], function(response)
             {
+                //  Only accept requests that has 200 =< status code < 300 
                 if (!(response.statusCode >= 200 && response.statusCode < 300))
                 {
                     reject(new Error('Failed to get data. Status code ' + response.statusCode));
@@ -36,6 +40,7 @@ function requestMultipleUrls(urls)
 
         });
 
+        //  Keep adding promises to a result array
         all_my_promises.push(one_promise);
     }   
 
